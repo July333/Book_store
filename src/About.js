f5388869279eae0ba2ea1566dfa3a1ff153a2d71
july333 <yulia.ivanov359@gmail.com>
2019-10-13 04:16:19 +0300

    forstart

---
+++ src/About.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+function About(props) {
+    
+    return (
+        <div>
+            <h1>about</h1>
+        </div>
+    )
+}
+
+export default About;
---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "jquery/dist/jquery.min.js";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-3">
+          hi
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
